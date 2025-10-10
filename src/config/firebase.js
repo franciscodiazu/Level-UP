819f6da1d101a60bda935f15103e50077b1b3ed8
Shaggy_ut <fra.diazu@duocuc.cl>
2025-10-10 09:25:13 -0300

    Registro de usuario
    Configuracion inicial del Index para validar datos de registro de usuario

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,6 +1,5 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 
